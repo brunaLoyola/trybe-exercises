6f56e3306433186aecc9dfa47e00e313cbb0637a
BrunaLoyola <brunloyola02@gmail.com>
2022-10-21 13:53:21 -0300

    Realização do exercício01 de fixação

---
+++ modulo01-fundamentos/bloco-04-introducao-a-javascript/dia-01-Javascript-primeiros-passos/exercicios-fixação/exercicio01/scripts.js
@@ -0,0 +1,16 @@
+// Declarando as variáveis
+const myName = "Bruna";
+const birthCity = "Paranaguá/PR";
+let birthYear = 2002;
+
+// Imprimindo os valores das variáveis
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+// Atribuindo um novo valor a variável birthYear e imprimindo o resultado
+birthYear = 2030;
+console.log(birthYear);
+
+// Tentando fazer a alteração da constante birthCity e imprimindo o resultado
+birthCity = "Matinhos";
