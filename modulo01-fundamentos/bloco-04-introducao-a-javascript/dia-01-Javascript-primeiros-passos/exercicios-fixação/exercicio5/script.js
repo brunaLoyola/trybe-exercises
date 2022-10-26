cf75fd87b45c86827cc8639c7a421841dc1348c6
BrunaLoyola <brunloyola02@gmail.com>
2022-10-26 13:23:14 -0300

    Exercício05 do dia 4.1, resolvido

---
+++ modulo01-fundamentos/bloco-04-introducao-a-javascript/dia-01-Javascript-primeiros-passos/exercicios-fixação/exercicio5/script.js
@@ -0,0 +1,17 @@
+let statusPessoa = "reprovada";
+
+switch (statusPessoa) {
+  case "aprovada":
+    console.log("Parabéns, você foi aprovada(o)!");
+    break;
+  case "reprovada":
+    console.log("Você foi reprovada(o)");
+    break;
+  case "lista":
+    console.log("Você está na nossa lista de espera");
+    break;
+
+  default:
+    console.log("Informação incorreta");
+    break;
+}
