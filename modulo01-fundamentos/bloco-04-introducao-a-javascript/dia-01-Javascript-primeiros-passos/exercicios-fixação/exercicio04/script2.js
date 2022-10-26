674a108f422c73924e9a1fc43f9eb2ec47b6639b
BrunaLoyola <brunloyola02@gmail.com>
2022-10-26 11:39:33 -0300

    Realização do script 2 do  exercicio 4

---
+++ modulo01-fundamentos/bloco-04-introducao-a-javascript/dia-01-Javascript-primeiros-passos/exercicios-fixação/exercicio04/script2.js
@@ -0,0 +1,16 @@
+// Declarando a váriavel
+let weekDay = "quarta-feira";
+
+// Realizando a condição e utilizando o operador OU (||)
+if (
+  weekDay == "segunda-feira" ||
+  weekDay == "terça-feira" ||
+  weekDay == "quarta-feira" ||
+  weekDay == "quinta-feira" ||
+  weekDay == "sexta-feira"
+) {
+  // Imprimindo a mensagem
+  console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+} else {
+  console.log("FINALMENTE, descanso merecido UwU");
+}
