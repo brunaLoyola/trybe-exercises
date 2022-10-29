b273a467808ed23c92d7c6a599416ebc35c81915
BrunaLoyola <brunloyola02@gmail.com>
2022-10-29 17:24:47 -0300

    Adicionando capa aos README dos exercícios

---
+++ modulo01-fundamentos/bloco-04-introducao-a-javascript/dia-01-Javascript-primeiros-passos/exercicios/exercicio01/script.js
@@ -0,0 +1,12 @@
+const a = 10;
+const b = 6;
+
+//Exercicio 01
+
+console.log("Adição: " + (a + b));
+console.log("Subtração: " + (a - b));
+console.log("Multiplicação: " + a * b);
+console.log("Divisão: " + a / b);
+console.log("Módulo: " + (a % b));
+
+// Exercicio 02
